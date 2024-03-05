05b454ede268b75e3db18224a94b24fce9981888
Dmitry <molokhovdmitry@gmail.com>
2024-03-05 16:38:28 +0300

    Create vm_startup.sh

---
+++ vm_startup.sh
@@ -0,0 +1,6 @@
+# Script for the automatic startup on a virtual machine.
+. /home/user/python_venv/social-stat/bin/activate
+cd /home/user/social-stat
+git pull
+pip install -r requirements.txt
+uvicorn src.main:app --host 0.0.0.0 --port 8000 > /home/user/log.txt 2>&1
